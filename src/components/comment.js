211c2ba19ec3ef218b17243b269d9b5c4693dc9d
webpropopuli <webpropopuli@gmail.com>
2018-09-10 17:35:13 -0400

    Done

--- src/components/comment.js
+++ src/components/comment.js
@@ -1,11 +1,36 @@
-import React, { Component } from 'react';
+import React from 'react';
+
+
 
 class Comment extends React.Component { 
+  constructor () {
+    super()
+    this.state = { charsTypedIn: 0, charsRemaining : 0 }
+  }
+  componentWillMount() {
+    this.setState({charsRemaining: this.props.maxLetters} )
+  }
+  handleChange = (event) => {
+    const len = event.target.value.length
+    if(len > this.props.maxLetters) {
+      console.log(event)
+      event.persist()
+      event.preventDefault()
+      event.stopPropagation()
+      event=null
+
+    }
+    else {
+    this.setState({charsRemaining: this.props.maxLetters - len} )
+    this.setState({charsTypedIn: len} )
+    }
+  }
+
     render() { 
       return ( 
         <div>
-           <textarea className="form-control" placeholder="Write a comment..." /> 
-           <small>140 Remaining</small> 
+           <textarea onChange={this.handleChange} className="form-control" placeholder="Your comment..." /> 
+           <small>{this.state.charsRemaining} chars left</small> 
         </div> 
         ) 
      } 
--- src/components/like.js
+++ src/components/like.js
@@ -1,35 +1,53 @@
-import React, { Component } from 'react';
+import React from 'react';
 
-class LikeIcon extends React.Component {
-    render() {
-      return (
-        <div>
-          <span className="fa-stack fa-sm">
-            <i className="fa fa-circle fa-stack-2x blue-icon" />
-            <i className="fa fa-thumbs-up fa-stack-1x fa-inverse" />
-          </span>
-        </div>
-      )
-    }
+const LikeIcon = () => {
+  return (
+    <div style={{display: 'inline'}}>
+      <span className="fa-stack fa-sm">
+        <i className="fa fa-circle fa-stack-2x blue-icon" />
+        <i className="fa fa-thumbs-up fa-stack-1x fa-inverse" />
+      </span>
+    </div>
+    )
   }
-  
+
   export default class Like extends React.Component {
+    constructor() { 
+      super() 
+      this.state = { liked: false } 
+     } 
+
+    toggleLike = () => { 
+      this.setState(prevState => ({ 
+        liked: !prevState.liked
+      }))
+     } 
+
     render() {
+      let thumb = 'fa fa-thumbs-o-up fa-3 align-middle'
       return (
         <div>
           {/* Include the LikeIcon subcomponent within the Like component*/}
-          <LikeIcon />
-          <hr />
-          <div>
-            <button type="button">
+          <span>
+            <button type="button" onClick={this.toggleLike}>
+              {(() => {
+                if(this.state.liked) { 
+                  //btnText = 'Hate ' 
+                  thumb = 'fa fa-thumbs-o-down fa-3 align-middle'
+                }
+              else {
+                 //btnText = 'Like '
+                 thumb = 'fa fa-thumbs-o-up fa-3 align-middle'
+                 }
+                })}
+              {this.state.liked ? 'Hate ' : 'Like '}
               <i
-                className="fa fa-thumbs-o-up fa-4 align-middle"
+                className={thumb}
                 aria-hidden="true"
               />
-              &nbsp;
-              <span className="align-middle">Like</span>
             </button>
-          </div>
+            {this.state.liked && <LikeIcon />}
+          </span>
         </div>
       )
     }
